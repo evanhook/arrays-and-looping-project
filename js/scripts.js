ac10cd797fa9287ad06c04b05a35cd44a0a85095
Elizabeth Van Hook <evanhook360@gmail.com>
2020-06-12 10:17:12 -0700

    testing logic in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,33 @@
 // Business Logic
-function beepboop(userInput) {
-  if (userInput < 0);
-    alert("please enter a number greater than 0")
-}
+
+//this excludes negative numbers and letters
+function robogerResponse(userInput) {
+  if (userInput < 0){
+    alert("please enter a number greater than 0");
+  }
+} 
+
 
 // User Interface
 $(document).ready(function(){
   $("form#numConverter").submit(function(event){
     event.preventDefault();
+    let output = [""]
     const userInput = parseInt($("input#userNum").val());
-    const output = beepboop(userInput); // beepboop will be the function that examines our exceptions/specs
     $("#numOutput").text(output);
+    
   });
 });
+
+
+
+/*testing loop here:
+const numbers = [1, 2, 3]
+const response = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+//let robogerResponse =[""];
+
+for (let index = 0; index <= userInput.length; index++) {
+let foundNum = userInput.find(element => element < 4);
+  console.log(foundNum);
+}
+*/
