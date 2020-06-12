d6dd988866c85ad8925845f7b208afe62fef6253
Elizabeth Van Hook <evanhook360@gmail.com>
2020-06-12 12:38:37 -0700

    testing logic for numbers that do not contain a 1, 2, or 3.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,14 @@ $(document).ready(function(){
 
     const userInput = $("input#userNum").val();
     const transformNum = ["1", "2", "3"];
+    const otherNum = ["0", "4", "5", "6", "7", "8", "9"]
     const greetingArray = ["Beep!", "Boop!", "Won't you be my neighbor?"];
     let output = [];
 
     function user(userInput){
-      if (userInput < 0){
-        alert("please enter a number greater than 0"); // add for letters
-      } 
+      if (userInput < 0 || userInput === NaN) {
+        output.push("Please enter a number greater than 0");
+      }
     }
     user(userInput);
 
@@ -22,10 +23,19 @@ $(document).ready(function(){
         output.push(greetingArray[1]);
       } else if (userInput.includes(transformNum[0])) {
         output.push(greetingArray[0]);;
-      }
+      } 
     })
 
 
     $("#numOutput").text(output);
   }); 
 });
+
+//if the user input does not contain a 123, show a count starting at 0 and replacing all 123 with the strings and the other numbers as numbers
+else if (userInput.includes(otherNum) && !userInput.includes(transformNum)) {
+  output.push(greetingArray[2]);;
+}
+
+//else if (userInput.includes(otherNum) && !userInput.includes(transformNum)) {
+ // output.push("Please enter a number");;
+//}
