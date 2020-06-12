3e4fbdd6920bb3dc8879026561225d010c132fbd
Elizabeth Van Hook <evanhook360@gmail.com>
2020-06-12 13:51:56 -0700

    continue working on incorporating inputs that do not contain a 1, 2, or 3

--- js/scripts.js
+++ js/scripts.js
@@ -23,10 +23,11 @@ $(document).ready(function(){
         output.push(greetingArray[1]);
       } else if (userInput.includes(transformNum[0])) {
         output.push(greetingArray[0]);;
-      } else if (!userInput.includes(transformNum[2,1,0])) {
-        output.push("placeholder");
+      } else if (userInput.includes(otherNum)){
+        while (userInput > 0 && !userInput.includes(transformNum[0,1,2]))
+          console.log(output);
       }
-    })
+    });
 
 
     $("#numOutput").text(output);
@@ -35,6 +36,17 @@ $(document).ready(function(){
 
 //if the user input does not contain a 123, show a count starting at 0 and replacing all 123 with the strings and the other numbers as numbers
 
-//else if (userInput.includes(otherNum) && !userInput.includes(transformNum)) {
- // output.push("Please enter a number");;
-//
+
+  //output.push(element)
+//}
+
+
+//else if (!userInput.includes(transformNum[2,1,0])) {
+ // output.push("placeholder");
+//}
+/*
+const otherNum = ["0", "4", "5", "6", "7", "8", "9"];
+let output = [];
+while (userInput > 0 && !userInput.includes(transformNum[0,1,2])) {
+  console.log(output)
+*/
