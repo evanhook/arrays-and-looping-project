c97ceeb93882d0f2c2c140a865b1f486ddc01209
Elizabeth Van Hook <evanhook360@gmail.com>
2020-06-15 21:22:11 -0700

    separate UI and business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,39 @@
+// Business Logic
+
+const transformNum = ["1", "2", "3"];
+const greetingArray = ["Beep!", "Boop!", "Won't you be my neighbor?"];
+let output = [];
+function robogerResponse(userInput, userValue) {
+  if (userInput < 0 || userInput === NaN) {
+    output.push("Please enter a number greater than 0");
+  } else if (userInput.includes(transformNum[2])) {
+    output.push(greetingArray[2]);
+  } else if (userInput.includes(transformNum[1])) {
+    output.push(greetingArray[1]);
+  } else if (userInput.includes(transformNum[0])) {
+    output.push(greetingArray[0]);
+  } for(let i=0; i<userValue; i++) {
+    //i=i.split("");
+    if (i.includes(transformNum[2])){
+      output.push(greetingArray[2]);
+     } else if (i.includes(transformNum[1])) {
+      output.push(greetingArray[1]);
+     } else if (i.includes(transformNum[0])) {
+      output.push (greetingArray[0])
+     } else {
+      output.push(i+1);
+     }
+  }
+};
+
+//User Interface
 $(document).ready(function(){
   $("form#numConverter").submit(function(event){
     event.preventDefault();
-
-    const userInput = $("input#userNum").val();
-    const transformNum = ["1", "2", "3"];
-    const otherNum = ["0", "4", "5", "6", "7", "8", "9"]
-    const greetingArray = ["Beep!", "Boop!", "Won't you be my neighbor?"];
-    let output = [];
-
-    const splitString = userInput.split(" ");
-    splitString.forEach(function(value){
-      if (userInput < 0 || userInput === NaN) {
-        output.push("Please enter a number greater than 0");
-      } else if (userInput.includes(transformNum[2])) {
-        output.push(greetingArray[2]);
-      } else if (userInput.includes(transformNum[1])) {
-        output.push(greetingArray[1]);
-      } else if (userInput.includes(transformNum[0])) {
-        output.push(greetingArray[0]);
-      } else if (!userInput.includes(transformNum)){
-        output.push("placeholder for number range");
-      }
-    });
-
+    output = [];
+    const userNum = $("input#userNum").val();
+    const splitString = userNum.split("");
+    robogerResponse(splitString, userNum);
     $("#numOutput").text(output);
   }); 
 });
